bca1e93cf52eb1804aefc8d834ca96d185af534d
Tanner Robinson <Rob16041@byui.edu>
2022-06-07 23:15:04 -0600

    made workout Equipment

--- lib/auxiliary/data.dart
+++ lib/auxiliary/data.dart
@@ -33,7 +33,8 @@ class Data {
       'preference_key': 'bench_press',
       'male_levels': [0.50, 0.625, 0.75, 1.00, 1.25, 1.5, 1.75, 1.875, 2.00],
       'female_levels': [0.25, 0.375, 0.50, 0.625, 0.75, 0.875, 1.0, 1.25, 1.50],
-      'focus': MuscleGroup.upper
+      'focus': MuscleGroup.upper,
+      'equipment' : [Equipment.barbell, Equipment.bench, Equipment.rack]
     },
     'squat': {
       'index': 1,
@@ -42,7 +43,8 @@ class Data {
       'key': 'squat',
       'male_levels': [0.75, 1.00, 1.25, 1.375, 1.50, 1.875, 2.25, 2.5, 2.75],
       'female_levels': [0.50, 0.625, 0.75, 1, 1.25, 1.375, 1.50, 1.75, 2.0],
-      'focus': MuscleGroup.legs
+      'focus': MuscleGroup.legs,
+      'equipment':[Equipment.barbell, Equipment.rack]
     },
     'dead_lift': {
       'index': 2,
@@ -51,7 +53,8 @@ class Data {
       'key': 'dead_lift',
       'male_levels': [1.00, 1.125, 1.25, 1.625, 2.00, 2.25, 2.50, 2.75, 3.00],
       'female_levels': [0.50, 0.75, 1.00, 1.125, 1.25, 1.50, 1.75, 2.125, 2.50],
-      'focus': MuscleGroup.upper
+      'focus': MuscleGroup.upper,
+      'Equipment':[Equipment.barbell]
     },
     'shoulder_press': {
       'index': 3,
@@ -60,7 +63,8 @@ class Data {
       'key': 'shoulder_press',
       'male_levels': [0.35, 0.45, 0.55, 0.675, 0.80, 0.925, 1.05, 1.20, 1.35],
       'female_levels': [0.20, 0.55, 0.35, 0.425, 0.50, 0.625, 0.75, 0.85, 0.95],
-      'focus': MuscleGroup.upper
+      'focus': MuscleGroup.upper,
+      'Equipment':[Equipment.barbell, Equipment.rack]
     },
     'pull_ups': {
       'index': 4,
@@ -69,7 +73,8 @@ class Data {
       'key': 'pull_ups',
       'male_levels': [0.0, 1.0, 4.0, 8.0, 13.0, 18.0, 24.0, 30.0, 36.0],
       'female_levels': [0.0, 0.0, 0.0, 1.0, 6.0, 10.0, 14.0, 19.0, 24.0],
-      'focus': MuscleGroup.upper
+      'focus': MuscleGroup.upper,
+      'Equipment':[Equipment.bar]
     },
     'dumbbell_bench_press': {
       'index': 5,
@@ -78,7 +83,8 @@ class Data {
       'key': 'dumbbell_bench_press',
       'male_levels': [0.20, 0.275, 0.35, 0.425, 0.50, 0.60, 0.70, 0.85, 0.95],
       'female_levels': [0.10, 0.15, 0.20, 0.25, 0.30, 0.40, 0.50, 0.60, 0.70],
-      'focus': MuscleGroup.upper
+      'focus': MuscleGroup.upper,
+      'Equipment':[Equipment.bench, Equipment.dumbbell]
     },
     'dumbbell_curl': {
       'index': 6,
@@ -87,7 +93,8 @@ class Data {
       'key': 'dumbbell_curl',
       'male_levels': [0.10, 0.125, 0.15, 0.225, 0.30, 0.375, 0.45, 0.55, 0.65],
       'female_levels': [0.05, 0.075, 0.10, 0.15, 0.20, 0.25, 0.30, 0.375, 0.45],
-      'focus': MuscleGroup.upper
+      'focus': MuscleGroup.upper,
+      'Equipment':[Equipment.dumbbell]
     },
     'barbell_curl': {
       'index': 7,
@@ -96,7 +103,8 @@ class Data {
       'key': 'barbell_curl',
       'male_levels': [0.20, 0.30, 0.40, 0.50, 0.60, 0.725, 0.85, 1.00, 1.15],
       'female_levels': [0.10, 0.15, 0.20, 0.30, 0.40, 0.50, 0.60, 0.725, 0.85],
-      'focus': MuscleGroup.upper
+      'focus': MuscleGroup.upper,
+      'Equipment':[Equipment.barbell, Equipment.rack]
     },
     'bent_over_row': {
       'index': 8,
@@ -105,7 +113,8 @@ class Data {
       'key': 'bent_over_row',
       'male_levels': [0.50, 0.625, 0.75, 0.875, 1.00, 1.25, 1.50, 1.625, 1.75],
       'female_levels': [0.25, 0.325, 0.40, 0.525, 0.65, 0.775, 0.90, 1.05, 1.20],
-      'focus': MuscleGroup.upper
+      'focus': MuscleGroup.upper,
+      'Equipment':[Equipment.barbell, Equipment.rack]
     },
     'push_ups': {
       'index': 9,
@@ -114,7 +123,8 @@ class Data {
       'key': 'push_ups',
       'male_levels': [0.0, 5.0, 16.0, 26.0, 40.0, 52.0, 68.0, 82.0, 100.0],
       'female_levels': [0.0, 1.0, .0, 10.0, 18.0, 25.0, 35.0, 44.0, 54.0],
-      'focus': MuscleGroup.upper
+      'focus': MuscleGroup.upper,
+      'Equipment':[Equipment.workoutPad]
     },
     'dumbbell_shoulder_press': {
       'index': 10,
@@ -123,7 +133,8 @@ class Data {
       'key': 'dumbbell_shoulder_press',
       'male_levels': [0.15, 0.20, 0.25, 0.325, 0.40, 0.475, 0.55, 1.30, 0.75],
       'female_levels': [0.10, 0.125, 0.15, 0.20, 0.25, 0.30, 0.35, 0.40, 0.45],
-      'focus': MuscleGroup.upper
+      'focus': MuscleGroup.upper,
+      'Equipment':[Equipment.dumbbell, Equipment.bench]
     },
     'front_squat': {
       'index': 11,
@@ -132,8 +143,18 @@ class Data {
       'key': 'front_squat',
       'male_levels': [0.75, 0.875, 1.00, 1.125, 1.25, 1.50, 1.75, 1.875, 2.00],
       'female_levels': [0.50, 0.625, 0.75, 0.875, 1.0, 1.125, 1.25, 1.375, 1.50],
-      'focus': MuscleGroup.legs
+      'focus': MuscleGroup.legs,
+      'Equipment':[Equipment.barbell, Equipment.rack]
     }
   };
 
+}
+
+enum Equipment{
+  barbell,
+  bench,
+  rack,
+  bar,
+  dumbbell,
+  workoutPad
 }
