aff46fb008c50fa1da95e0bd0aa1de666f9ff1a2
BuildTools <unconfigured@null.spigotmc.org>
2022-06-16 16:52:12 -0600

    Added exercise instruction modal

--- lib/workout/workout.dart
+++ lib/workout/workout.dart
@@ -4,6 +4,7 @@ import 'package:myjym/workout/plan_workout.dart';
 
 import '../auxiliary/data.dart';
 import '../auxiliary/modal.dart';
+import 'instruction.dart';
 
 class Workout extends StatelessWidget {
   Workout({Key? key, required this.day}) : super(key: key);
@@ -28,7 +29,16 @@ class Workout extends StatelessWidget {
     );
   }
 
-  Widget _workoutDisplay({required workout}) {
+  static const _backgroundColor = Colors.white;
+  static const _margin = EdgeInsets.fromLTRB(8, 4, 8, 4);
+  static const _padding = EdgeInsets.fromLTRB(8, 4, 8, 4);
+  static const _borderRadius = BorderRadius.all(Radius.circular(20));
+  static var _border = Border.all(
+    width: 2,
+    color: Styles.brown,
+  );
+
+  Widget _workoutDisplay({required workout, required context}) {
     return SingleChildScrollView(
       child: Container(
         child: Column(
@@ -45,47 +55,67 @@ class Workout extends StatelessWidget {
               style: Styles.header2,
             ),
             ...(workout['exercises']).map((exercise) {
-              return Column(
-                children: [
-                  Styles.horizontalRule(),
-                  Text(
-                    Data.exerciseInfo[exercise['type']]!['name'] as String,
-                    style: Styles.header2,
+              return Container(
+                  margin: _margin,
+                  padding: _padding,
+                  decoration: BoxDecoration(
+                    color: _backgroundColor,
+                    borderRadius: _borderRadius,
+                    border: _border,
                   ),
-                  ...(exercise['sets']).map((set) {
-                    return Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        set['weight'] == 0.0
-                            ? const Text('')
-                            : Text(
-                                set['weight'].toStringAsFixed(1),
-                                style: Styles.header3,
-                              ),
-                        set['weight'] == 0.0
-                            ? const Text(
-                                'Body Weight * ',
-                                style: Styles.header3,
-                              )
-                            : const Text(
-                                ' lbs * ',
-                                style: Styles.header3,
-                              ),
-                        Text(
-                          set['reps'].toString(),
-                          style: Styles.header3,
+                  child: Stack(
+                    children: [
+                      Column(
+                        children: [
+                          Text(
+                            Data.exerciseInfo[exercise['type']]!['name']
+                                as String,
+                            style: Styles.header2,
+                          ),
+                          ...(exercise['sets']).map((set) {
+                            return Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                set['weight'] == 0.0
+                                    ? const Text('')
+                                    : Text(
+                                        set['weight'].toStringAsFixed(1),
+                                        style: Styles.header3,
+                                      ),
+                                set['weight'] == 0.0
+                                    ? const Text(
+                                        'Body Weight * ',
+                                        style: Styles.header3,
+                                      )
+                                    : const Text(
+                                        ' lbs * ',
+                                        style: Styles.header3,
+                                      ),
+                                Text(
+                                  set['reps'].toString(),
+                                  style: Styles.header3,
+                                ),
+                                const Text(
+                                  ' reps',
+                                  style: Styles.header3,
+                                ),
+                              ],
+                            );
+                          }).toList(),
+                        ],
+                      ),
+                      Positioned(
+                        bottom: 0,
+                        right: 0,
+                        child: FloatingActionButton.small(
+                          onPressed: () => Modal.open(
+                              context: context, child: Instruction(exercise: exercise['type'])),
+                          child: const Icon(Icons.question_mark_rounded),
                         ),
-                        const Text(
-                          ' reps',
-                          style: Styles.header3,
-                        ),
-                      ],
-                    );
-                  }).toList(),
-                ],
-              );
+                      ),
+                    ],
+                  ));
             }).toList(),
-            Styles.horizontalRule(),
             Text(
               'Cool down for ' +
                   (workout['duration_cooldown']).toInt().toString() +
@@ -108,7 +138,8 @@ class Workout extends StatelessWidget {
               )
             : getWorkout(day).isNotEmpty
                 ? _workoutDisplay(
-                    workout: getWorkout(day)[0] as Map<String, Object>)
+                    workout: getWorkout(day)[0] as Map<String, Object>,
+                    context: context)
                 : _noWorkout(context: context));
   }
 }
