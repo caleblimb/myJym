6e343b868fb18d45b3c942e6d48efa514124c384
Tanner Robinson <Rob16041@byui.edu>
2022-07-08 10:44:22 -0600

    New Getting started menu on first load no modal

--- lib/workout/workout_view.dart
+++ lib/workout/workout_view.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:myjym/workout/deprecated_workout.dart';
 import 'package:myjym/workout/interactive_page_view_workout.dart';
 
-import '../auxiliary/modal.dart';
 import '../quickstart/quickstart.dart';
 import '../auxiliary/preference_manager.dart';
 
@@ -18,31 +16,25 @@ class _WorkoutViewState extends State<WorkoutView> {
   @override
   Widget build(BuildContext context) {
     if (!PreferenceManager.getSetup()) {
-      return Container(
-        child: Column(
+      return Expanded(
+        child: new ListView(
+          shrinkWrap: false,
           children: [
-            Container(
-              child: const Image(
-                image: AssetImage('assets/ui/myJym.png'),
-              ),
-              margin: const EdgeInsets.all(16),
-            ),
-            SizedBox(
-              width: double.infinity,
-              height: 64,
-              child: ElevatedButton(
-                onPressed: () async {
-                  await Modal.open(
-                    context: context,
-                    child: const QuickStart(),
-                  );
+            QuickStart(),
+            ElevatedButton(
+                onPressed: (){
+                  PreferenceManager.setSetup(true);
                   setState(() {});
                 },
-                child: const Text("Get Started"),
-              ),
+                child: Text("Done")
             ),
           ],
         ),
+        // Column(
+        //   children: [
+        //     QuickStart(),
+        //   ],
+        // ),
       );
     } else {
       return Container(
