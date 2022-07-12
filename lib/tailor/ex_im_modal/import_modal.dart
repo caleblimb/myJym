98c8ad6f8537dc843922af983306ce08e88bb602
Tanner Robinson <Rob16041@byui.edu>
2022-07-11 21:48:24 -0600

    warning fixes

--- lib/tailor/ex_im_modal/import_modal.dart
+++ lib/tailor/ex_im_modal/import_modal.dart
@@ -1,9 +1,5 @@
-import 'package:clipboard/clipboard.dart';
 import 'package:flutter/material.dart';
-import 'package:myjym/auxiliary/preference_manager.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
-import '../../auxiliary/styles.dart';
 
 class Import extends StatefulWidget {
   const Import({Key? key}) : super(key: key);
