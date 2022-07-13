e4ba75c728c6857938d9984280e141a0e9adf567
Jeff Pitcher <pit19004@byui.edu>
2022-07-12 23:36:06 -0600

    added 7 more workout to leg workouts

--- lib/workout/instruction.dart
+++ lib/workout/instruction.dart
@@ -8,121 +8,10 @@ class Instruction extends StatelessWidget {
 
   final Exercise exercise;
 
-  static final Map<Object, Map<String, Object>> exerciseInstructions = {
-    Exercise.benchPress: {
-      'title': 'Bench Press',
-      'tips': [
-        'Lay back flat down on bench',
-        'Place feet on ground',
-        'Put both arms up at the same time because we like to party a lot',
-        'Don\'t over exert yourself',
-      ],
-    },
-    Exercise.squat: {
-      'title': 'Squat',
-      'tips': [
-        'Rest bar on shoulders',
-        'Place feet same distance as shoulders',
-        'Place hands on bar about a foot away from shoulders',
-        'Don\'t over exert yourself',
-      ],
-    },
-    Exercise.deadLift: {
-      'title': 'Deadlift',
-      'tips': [
-        'Keep back straight',
-        'Pop chest out',
-        'Bend your knees',
-        'Don\'t over exert yourself',
-      ],
-    },
-    Exercise.shoulderPress: {
-      'title': 'Shoulder Press',
-      'tips': [
-        'Rest bar front of shoulders',
-        'Place feet same distance as shoulders',
-        'Place hands on bar about a foot away from shoulders',
-        'Don\'t over exert yourself',
-      ],
-    },
-    Exercise.pullUps: {
-      'title': 'Pull-Up',
-      'tips': [
-        'Rest bar on shoulders',
-        'Place feet same distance as shoulders',
-        'Place hands on bar about a foot away from shoulders',
-        'Don\'t over exert yourself',
-      ],
-    },
-    Exercise.dumbbellBenchPress: {
-      'title': 'Dumbbell Bench Press',
-      'tips': [
-        'Rest bar on shoulders',
-        'Place feet same distance as shoulders',
-        'Place hands on bar about a foot away from shoulders',
-        'Don\'t over exert yourself',
-      ],
-    },
-    Exercise.dumbbellCurl: {
-      'title': 'Dumbbell Curl',
-      'tips': [
-        'Rest bar on shoulders',
-        'Place feet same distance as shoulders',
-        'Place hands on bar about a foot away from shoulders',
-        'Don\'t over exert yourself',
-      ],
-    },
-    Exercise.barbellCurl: {
-      'title': 'Barbell Curl',
-      'tips': [
-        'Keep back straight',
-        'Place feet shoulder distance',
-        'Don\'t rock with barbell; Use LESS weight',
-        'Don\'t over exert yourself',
-      ],
-    },
-    Exercise.bentOverRow: {
-      'title': 'BentOver Row',
-      'tips': [
-        'Rest bar on shoulders',
-        'Place feet same distance as shoulders',
-        'Place hands on bar about a foot away from shoulders',
-        'Don\'t over exert yourself',
-      ],
-    },
-    Exercise.pushUps: {
-      'title': 'Push-Up',
-      'tips': [
-        'Rest bar on shoulders',
-        'Place feet same distance as shoulders',
-        'Place hands on bar about a foot away from shoulders',
-        'Don\'t over exert yourself',
-      ],
-    },
-    Exercise.dumbbellShoulderPress: {
-      'title': 'Dumbbell Shoulder Press',
-      'tips': [
-        'Rest bar on shoulders',
-        'Place feet same distance as shoulders',
-        'Place hands on bar about a foot away from shoulders',
-        'Don\'t over exert yourself',
-      ],
-    },
-    Exercise.frontSquat: {
-      'title': 'Front Squat',
-      'tips': [
-        'Rest bar on shoulders',
-        'Place feet same distance as shoulders',
-        'Place hands on bar about a foot away from shoulders',
-        'Don\'t over exert yourself',
-      ],
-    },
-  };
-
   static Widget exerciseTips({required exerciseType, TextStyle? textStyle, double? iconTop}) {
     return Column(
       children: [
-        ...(exerciseInstructions[exerciseType]!['tips'] as List<String>).map(
+        ...(Data.exerciseInstructions[exerciseType]!['tips'] as List<String>).map(
           (tip) {
             return Stack(children: [
               Positioned(
@@ -158,7 +47,7 @@ class Instruction extends StatelessWidget {
         child: Column(
           children: [
             Text(
-              exerciseInstructions[exercise]!['title'] as String,
+              Data.exerciseInstructions[exercise]!['title'] as String,
               style: Styles.header1,
             ),
             Container(
